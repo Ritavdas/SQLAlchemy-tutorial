06c4fbb76db109499a32aefc930e9eae92d2b738
ritavdas <ritavdas@gmail.com>
2022-05-31 17:17:16 +0530

    add files

---
+++ delete.py
@@ -0,0 +1,12 @@
+from main import Session, engine, User
+
+
+local_session = Session(bind=engine)
+
+
+user_to_delete = local_session.query(User).filter(
+    User.username == "Chigga").first()
+
+local_session.delete(user_to_delete)
+
+local_session.commit()
---
+++ update.py
@@ -0,0 +1,12 @@
+from main import Session, engine, User
+
+
+local_session = Session(bind=engine)
+
+user_to_update = local_session.query(
+    User).filter(User.username == 'jerry').first()
+
+user_to_update.username = "jonathan"
+user_to_update.email = "jonathan@company.com"
+
+local_session.commit()
